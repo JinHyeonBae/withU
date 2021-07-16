f01a59988b85787ab57ffaead9f12a71d68f10f0
DESKTOP-BTPSS26\BJH <bqwert36@hanmail.net>
2021-07-17 03:37:05 +0900

    dialogflow와 연동 코드

--- Server/connect.py
+++ Server/connect.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 import json
@@ -6,12 +5,10 @@ import json
 keyfile = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
 def readKeyfile() :
+
     with open(keyfile, 'r') as f:
         json_data = json.load(f)
     
-    data = json.dumps(json_data["project_Id"])
+    data = json.dumps(json_data["project_id"])
+    print(data)
     return data
-
-
-def webhookResponse (text):
-    print(text)
